b2ce97b8d9f45716636e8c8c1d9f66ae9d3af757
fariya-sultana <fariya.sultana.dev@gmail.com>
2025-06-17 21:11:25 +0700

    final commit

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-admin-sdk-service-key.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
